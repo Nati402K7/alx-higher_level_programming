9dfe76607837045edcf7507d52891bf764ebe56d
Nati402K7 <natnaeleyasu402@gmail.com>
2024-01-03 12:55:02 -0800

    Update

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for j in range(i + 1, 10):
-        if i == 8 and j == 9:
-            print('89')
-        else:
-            print('{}{},'.format(x, y), end='')
+for i in range(10):
+    for j in range(i, 10):
+        if i < j:
+            print("{:d}{:d}".format(i, j),
+                    end="\n" if i == 8 and j == 9 else ", ")
