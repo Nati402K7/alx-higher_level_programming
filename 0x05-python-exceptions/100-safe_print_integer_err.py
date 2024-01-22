5a8444e35b207e2e41f924e334e7551ae7434325
Nati402K7 <natnaeleyasu402@gmail.com>
2024-01-22 14:08:01 -0800

    update

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,10 +2,10 @@
 import sys
 
 def safe_print_integer_err(value):
-    i = True
+    is_int = True
     try:
         print("{:d}".format(value))
     except Exception as e:
         print("Exception:", e, file=sys.stderr)
-        i = False
-    return i
+        is_int = False
+    return is_int
