5f4b191bcf05da521a9329f5ac8507adaba76ddd
Nati402K7 <natnaeleyasu402@gmail.com>
2024-01-22 14:00:18 -0800

    update

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,7 +4,7 @@ def safe_print_integer_err(value):
     i = True
     try:
         print("{:d}".format(value))
-    except Exception as j:
-        print("Exception:", j, file=sys.stderr)
+    except Exception as e:
+        print("Exception:", e, file=sys.stderr)
         i = False
     return i
--- 0x05-python-exceptions/README.md
+++ 0x05-python-exceptions/README.md
@@ -5,3 +5,7 @@ Integers division with debug
 Division a list
 Raise exception
 Raise a message
+Safe integer print with error message
+Safe function
+ByteCode -> Python #4
+CPython #2: PyFloatObject
